ae2f67dbd68df6f5e2d147198f2f783bb587dae2
Losted-melody <41710742+Losted-melody@users.noreply.github.com>
2019-03-21 14:05:01 +0800

    Update kNN.py

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -16,21 +16,21 @@ import operator
 from os import listdir
 
 def classify0(inX, dataSet, labels, k):
-    dataSetSize = dataSet.shape[0]
-    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    dataSetSize = dataSet.shape[0]#得到数据集的行数
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet#重复的行数和列数
     sqDiffMat = diffMat**2
-    sqDistances = sqDiffMat.sum(axis=1)
+    sqDistances = sqDiffMat.sum(axis=1)#在行上求和
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()     
+    sortedDistIndicies = distances.argsort()#输出从小到大的索引     
     classCount={}          
     for i in range(k):
-        voteIlabel = labels[sortedDistIndicies[i]]
-        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+        voteIlabel = labels[sortedDistIndicies[i]]#输出距离最近的前k个标签
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1#输出距离最近的前k个标签：次数的键值对
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)#根据第二个域进行排序，也就是value标签，降序排列，这种排序写法针对字典，第二个参数key有多种选择
+    return sortedClassCount[0][0]#返回值为出现频率次数第一的标签
 
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])#numpy中的自带函数array，将列表转化为自身里的列表
     labels = ['A','A','B','B']
     return group, labels
 
@@ -39,30 +39,30 @@ def file2matrix(filename):
     fr = open(filename)
     arrayOLines = fr.readlines()
     numberOfLines = len(arrayOLines)            #get the number of lines in the file
-    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return,numberOfLines行，3列，初始化为0
     classLabelVector = []                       #prepare labels return   
     index = 0
     for line in arrayOLines:
-        line = line.strip()
-        listFromLine = line.split('\t')
+        line = line.strip()         #去除头尾空格
+        listFromLine = line.split('\t')         #以制表符为分隔依据，返回分割后的字符串列表。
         returnMat[index,:] = listFromLine[0:3]
-        if(listFromLine[-1].isdigit()):
+        if(listFromLine[-1].isdigit()):         #检测字符串是否只由数字组成。
             classLabelVector.append(int(listFromLine[-1]))
         else:
-            classLabelVector.append(love_dictionary.get(listFromLine[-1]))
+            classLabelVector.append(love_dictionary.get(listFromLine[-1]))          #get是根据键得到值的意思
         index += 1
-    return returnMat,classLabelVector
+    return returnMat,classLabelVector           #返回指标和分类结果
 
     
 def autoNorm(dataSet):
-    minVals = dataSet.min(0)
+    minVals = dataSet.min(0)        #min(0)返回该矩阵中每一列的最小值
     maxVals = dataSet.max(0)
     ranges = maxVals - minVals
-    normDataSet = zeros(shape(dataSet))
+    normDataSet = zeros(shape(dataSet))         #n行m列
     m = dataSet.shape[0]
     normDataSet = dataSet - tile(minVals, (m,1))
     normDataSet = normDataSet/tile(ranges, (m,1))   #element wise divide
-    return normDataSet, ranges, minVals
+    return normDataSet, ranges, minVals         #返回标准值，范围，最小值
    
 def datingClassTest():
     hoRatio = 0.50      #hold out 10%
@@ -72,7 +72,7 @@ def datingClassTest():
     numTestVecs = int(m*hoRatio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)   #第i行所有数据，第一半行所有数据与分类
         print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
         if (classifierResult != datingLabels[i]): errorCount += 1.0
     print "the total error rate is: %f" % (errorCount/float(numTestVecs))
@@ -81,7 +81,7 @@ def datingClassTest():
 def classifyPerson():
     resultList = ['not at all', 'in small doses', 'in large doses']
     percentTats = float(raw_input(\
-                                  "percentage of time spent playing video games?"))
+                                  "percentage of time spent playing video games?")) #raw_input() 将所有输入作为字符串看待，返回字符串类型。
     ffMiles = float(raw_input("frequent flier miles earned per year?"))
     iceCream = float(raw_input("liters of ice cream consumed per year?"))
     datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
@@ -89,7 +89,7 @@ def classifyPerson():
     inArr = array([ffMiles, percentTats, iceCream, ])
     classifierResult = classify0((inArr - \
                                   minVals)/ranges, normMat, datingLabels, 3)
-    print "You will probably like this person: %s" % resultList[classifierResult - 1]
+    print "You will probably like this person: %s" % resultList[classifierResult - 1]           #返回预测结果
     
 def img2vector(filename):
     returnVect = zeros((1,1024))
@@ -97,20 +97,20 @@ def img2vector(filename):
     for i in range(32):
         lineStr = fr.readline()
         for j in range(32):
-            returnVect[0,32*i+j] = int(lineStr[j])
+            returnVect[0,32*i+j] = int(lineStr[j])          #32*32=1024，得到每个文件的值排成一列
     return returnVect
 
 def handwritingClassTest():
     hwLabels = []
-    trainingFileList = listdir('trainingDigits')           #load the training set
+    trainingFileList = listdir('trainingDigits')           #load the training set，os.listdir() 方法用于返回指定的文件夹包含的文件或文件夹的名字的列表。这个列表以字母顺序。 它不包括 '.' 和'..' 即使它在文件夹中。
     m = len(trainingFileList)
     trainingMat = zeros((m,1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
         fileStr = fileNameStr.split('.')[0]     #take off .txt
-        classNumStr = int(fileStr.split('_')[0])
+        classNumStr = int(fileStr.split('_')[0])            #文件里面的数字是几
         hwLabels.append(classNumStr)
-        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+        trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)            #得到每一个文件的一列排放
     testFileList = listdir('testDigits')        #iterate through the test set
     errorCount = 0.0
     mTest = len(testFileList)
@@ -119,7 +119,7 @@ def handwritingClassTest():
         fileStr = fileNameStr.split('.')[0]     #take off .txt
         classNumStr = int(fileStr.split('_')[0])
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
-        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)     #用距离来判别频率最高的那个
         print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
         if (classifierResult != classNumStr): errorCount += 1.0
     print "\nthe total number of errors is: %d" % errorCount
