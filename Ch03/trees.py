f695ad4958df7bf33c4a745194410d22a01bcf10
Losted-melody <41710742+Losted-melody@users.noreply.github.com>
2019-03-21 16:17:54 +0800

    Update trees.py

--- Ch03/trees.py
+++ Ch03/trees.py
@@ -20,7 +20,7 @@ def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
     for featVec in dataSet: #the the number of unique elements and their occurance
-        currentLabel = featVec[-1]
+        currentLabel = featVec[-1]  #用最后一个当标签计算香农熵
         if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
     shannonEnt = 0.0
@@ -29,7 +29,7 @@ def calcShannonEnt(dataSet):
         shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
     
-def splitDataSet(dataSet, axis, value):
+def splitDataSet(dataSet, axis, value): #从关键词开始切分，去掉关键词返回另一个列表
     retDataSet = []
     for featVec in dataSet:
         if featVec[axis] == value:
@@ -43,26 +43,26 @@ def chooseBestFeatureToSplit(dataSet):
     baseEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0; bestFeature = -1
     for i in range(numFeatures):        #iterate over all the features
-        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
-        uniqueVals = set(featList)       #get a set of unique values
+        featList = [example[i] for example in dataSet]#create a list of all the examples of this feature，将dataSet中的数据先按行依次放入example中，然后取得example中的example[i]元素，放入列表featList中
+        uniqueVals = set(featList)       #get a set of unique values，去重
         newEntropy = 0.0
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet)/float(len(dataSet))
-            newEntropy += prob * calcShannonEnt(subDataSet)     
+            newEntropy += prob * calcShannonEnt(subDataSet) #各个值的权重乘以子集香农熵   
         infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
         if (infoGain > bestInfoGain):       #compare this to the best gain so far
             bestInfoGain = infoGain         #if better than current best, set to best
             bestFeature = i
-    return bestFeature                      #returns an integer
+    return bestFeature                      #returns an integer，从哪个标签切香农熵最大
 
 def majorityCnt(classList):
     classCount={}
     for vote in classList:
         if vote not in classCount.keys(): classCount[vote] = 0
         classCount[vote] += 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True) #sorted返回值是[(),()]这种
+    return sortedClassCount[0][0]   #返回出现频率最高的值key
 
 def createTree(dataSet,labels):
     classList = [example[-1] for example in dataSet]
@@ -72,22 +72,22 @@ def createTree(dataSet,labels):
         return majorityCnt(classList)
     bestFeat = chooseBestFeatureToSplit(dataSet)
     bestFeatLabel = labels[bestFeat]
-    myTree = {bestFeatLabel:{}}
+    myTree = {bestFeatLabel:{}} #选出了最好的分类标签
     del(labels[bestFeat])
     featValues = [example[bestFeat] for example in dataSet]
     uniqueVals = set(featValues)
     for value in uniqueVals:
         subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
-        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels) #用递归的方法生成树myTree = {'xinlingshan':{'lijian':'taolin'}}  myTree['xinlingshan']['lijian']
     return myTree                            
     
 def classify(inputTree,featLabels,testVec):
-    firstStr = inputTree.keys()[0]
-    secondDict = inputTree[firstStr]
-    featIndex = featLabels.index(firstStr)
+    firstStr = inputTree.keys()[0]  #父亲结点，倒着搞的，草拟吗，反人类
+    secondDict = inputTree[firstStr]    #父亲节点以下的所有节点
+    featIndex = featLabels.index(firstStr)  #index() 方法检测字符串中是否包含子字符串 str,返回所在位置，列表也能搞
     key = testVec[featIndex]
-    valueOfFeat = secondDict[key]
-    if isinstance(valueOfFeat, dict): 
+    valueOfFeat = secondDict[key]   #看看key是0还是1，然后选择分支
+    if isinstance(valueOfFeat, dict):   #判断对象是否是字典类型，是字典类型则为叶子结点
         classLabel = classify(valueOfFeat, featLabels, testVec)
     else: classLabel = valueOfFeat
     return classLabel
@@ -95,11 +95,11 @@ def classify(inputTree,featLabels,testVec):
 def storeTree(inputTree,filename):
     import pickle
     fw = open(filename,'w')
-    pickle.dump(inputTree,fw)
+    pickle.dump(inputTree,fw)   # 函数的功能：将obj对象序列化存入已经打开的file中。
     fw.close()
     
 def grabTree(filename):
     import pickle
     fr = open(filename)
-    return pickle.load(fr)
+    return pickle.load(fr)  #函数的功能：将file中的对象序列化读出。
     
